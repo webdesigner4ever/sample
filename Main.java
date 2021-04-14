79f429857ce607788900881505235e8f0605ee8f
webdesigner4ever <63400538+webdesigner4ever@users.noreply.github.com>
2021-04-15 03:00:28 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     int x = 20;
     int y = 18;
     if (x > y) {
-      System.out.println("y is lesser than x");
+      System.out.println("y is not greater than x");
     }  
   }
 }
